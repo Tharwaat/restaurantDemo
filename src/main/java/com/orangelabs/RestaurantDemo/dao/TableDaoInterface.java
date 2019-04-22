57b1c2b6bf057ce98fd0465131c46761c62d3f84
Tharwat <a.tharwat@theelephant.tech>
2019-04-22 11:36:31 +0200

    Get available tables query

--- src/main/java/com/orangelabs/RestaurantDemo/dao/TableDaoInterface.java
+++ src/main/java/com/orangelabs/RestaurantDemo/dao/TableDaoInterface.java
@@ -9,7 +9,11 @@ import org.springframework.data.jpa.repository.Query;
 import com.orangelabs.RestaurantDemo.entity.TableEntity;
 
 public interface TableDaoInterface extends JpaRepository<TableEntity, Integer> {
-	@Query(value="SELECT * FROM restaurant_table AS a left join reservation As b ON a.id = b.table_id WHERE b.id IS NULL AND b.date != ?1", 
+	@Query(value="SELECT * FROM restaurant_table AS a "
+				 + "LEFT JOIN reservation AS b ON a.id = b.table_id WHERE b.table_id IS NULL "
+				 + "UNION "
+				 + "SELECT * FROM restaurant_table AS a "
+				 + "INNER JOIN reservation AS b ON a.id = b.table_id WHERE  b.date !=  ?1", 
 				nativeQuery = true)
 	public List<TableEntity> getAvailableTables(Date availableDate);
 }
